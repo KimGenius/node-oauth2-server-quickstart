0981e045d60a30b320c0ac6585ba786340ad517a
KimGenius <geniusk1047@naver.com>
2018-01-12 18:13:10 +0900

    왜 나온지 5달도 넘은게 example이 없나요?

--- index.js
+++ index.js
@@ -2,9 +2,38 @@ const express = require('express')
 const app = express()
 const OAuth2Server = require('oauth2-server')
 app.get('/', function (req, res) {
-  const oauth = new OAuth2Server({
-    model: require('./model')
+  const oauth = new OAuth2Server({ model: { generateAccessToken: function() {} } })
+  const Request = OAuth2Server.Request;
+  const Response = OAuth2Server.Response;
+
+  let request = new Request({
+    body: {},
+    headers: { 'Authorization': 'Bearer 2101749124594a59cf981e2a60990636960bb7c2' },
+    method: {},
+    query: {}
+  });
+  let response = new Response(res);
+
+  oauth.authenticate(request, response)
+  .then((token) => {
+    console.log(token)
   })
+  .catch((err) => {
+    console.error(err.message)
+  });
+  // const AccessDeniedError = require('oauth2-server/lib/errors/access-denied-error');
+
+  // oauth.authorize(request, response)
+  //   .then((code) => {
+  //   // The resource owner granted the access request.
+  // })
+  // .catch((err) => {
+  //   if (err instanceof AccessDeniedError) {
+  //   // The resource owner denied the access request.
+  //   } else {
+  //     // Access was not granted due to some other error condition.
+  //   }
+  // });
 })
 
 app.listen(3030, function() {
